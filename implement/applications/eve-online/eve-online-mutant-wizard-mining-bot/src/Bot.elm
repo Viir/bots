584d7d67a66c7796db160e67096b079373dadf5e
Michael Rätzel <Viir@Viir.de>
2019-10-15 17:50:03 +0000

    Implement MutantWizards EVE Online mining bot
    Implement the EVE Online mining bot described by MutantWizard.
    This new bot is based on a combination of the travel bot from `eve-online-warp-to-0-autopilot` and the sanderling framework from the directory `eve-online-sanderling-framework`.
    During the development, a simplified intermediate language to model the bot program emerged. The `Bot.elm` module uses this language to model the bot, composed of multiple sequences of steps. Conditional jumps (using the `JumpToSequence` instruction) support modeling branching. A new memory reading from the game client is taken for the execution of each step. In each step, the decision on how to proceed is made solely based on the current impression of the environment. This impression of the bots environment is modeled in the `MemoryMeasurement`.
    Control flow is modeled using the `BotProgramStepResult` type. The `Wait` case means the same step will be executed again after a taking the next memory reading from the game client.

---
+++ implement/applications/eve-online/eve-online-mutant-wizard-mining-bot/src/Bot.elm
@@ -0,0 +1,458 @@
+{- This is an asteroid mining bot for EVE Online, based on a process suggested by MutantWizard:
+    + https://forum.botengine.org/t/how-to-automate-mining-asteroids-in-eve-online/628/43?u=viir
+    + https://forum.botengine.org/t/how-to-automate-mining-asteroids-in-eve-online/628/70?u=viir
+
+    Setup instructions:
+    + Set the UI language to English.
+    + In Overview window, make asteroids visible.
+    + Set the Overview window to sort objects in space by distance with the nearest entry at the top.
+    + In the Inventory window select the ‘List’ view.
+    + Setup inventory window so that Ore Hold is always selected.
+    + In the ship UI, hide all modules which are no miners.
+    + Enable the info panel ‘System info’.
+    + Create bookmark 'mining' for the asteroid belt.
+    + Create bookmark 'unload' for the station.
+
+   bot-catalog-tags:eve-online,mining
+-}
+
+
+module Bot exposing
+    ( State
+    , initState
+    , processEvent
+    )
+
+import BotEngine.Interface_To_Host_20190808 as InterfaceToHost
+import Sanderling exposing (MouseButton(..), centerFromRegion, effectMouseClickAtLocation)
+import SanderlingMemoryMeasurement
+    exposing
+        ( InfoPanelRouteRouteElementMarker
+        , MaybeVisible(..)
+        , MemoryMeasurementShipUi
+        , OverviewWindowEntry
+        , ShipUiModule
+        , UIElement
+        , maybeNothingFromCanNotSeeIt
+        )
+import SimpleSanderling exposing (BotEventAtTime, BotRequest(..))
+
+
+type alias MemoryMeasurement =
+    SanderlingMemoryMeasurement.MemoryMeasurementReducedWithNamedNodes
+
+
+type BotProgramSequenceName
+    = TravelToAsteroidBelt
+    | MineInBelt
+    | TravelToStation
+    | TransferFromOreHold
+
+
+type BotProgramStepResult
+    = ApplyEffectAndContinue Sanderling.EffectOnWindowStructure
+    | Continue
+    | JumpToSequence BotProgramSequenceName
+    | Wait
+
+
+type alias BotProgramStep =
+    ( String, MemoryMeasurement -> BotProgramStepResult )
+
+
+type alias BotProgramSequence =
+    { steps : List BotProgramStep
+    , continueWith : BotProgramSequenceName
+    }
+
+
+type alias SimpleState =
+    { remainingSequence : BotProgramSequence
+    }
+
+
+type alias State =
+    SimpleSanderling.StateIncludingSetup SimpleState
+
+
+asteroidBookmarkName : String
+asteroidBookmarkName =
+    "mining"
+
+
+stationBookmarkName : String
+stationBookmarkName =
+    "unload"
+
+
+generalStepDelayMilliseconds : Int
+generalStepDelayMilliseconds =
+    2000
+
+
+getProgramSequence : BotProgramSequenceName -> BotProgramSequence
+getProgramSequence sequenceName =
+    case sequenceName of
+        TravelToAsteroidBelt ->
+            { steps =
+                [ ( "Undock ship: open menu"
+                  , activeShipUiElementFromInventoryWindow
+                        >> Maybe.map (clickLocationOnInventoryShipEntry >> effectMouseClickAtLocation MouseButtonRight >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "Undock ship: click menu entry"
+                  , menuEntryInLastMenuContainingTextIgnoringCase "Undock"
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "Travel to asteroid belt: open solar system menu"
+                  , .infoPanelCurrentSystem
+                        >> maybeNothingFromCanNotSeeIt
+                        >> Maybe.map (.listSurroundingsButton >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "Travel to asteroid belt: open bookmark"
+                  , menuEntryInLastMenuContainingTextIgnoringCase asteroidBookmarkName
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "Travel to asteroid belt: click menu entry to start warp"
+                  , menuEntryInLastMenuContainingTextIgnoringCase "Warp to Location Within 0 m"
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                ]
+            , continueWith = MineInBelt
+            }
+
+        MineInBelt ->
+            { steps =
+                [ ( "when warp ends"
+                  , .shipUi
+                        >> maybeNothingFromCanNotSeeIt
+                        >> Maybe.map isShipWarpingOrJumping
+                        >> Maybe.withDefault True
+                        >> mapBoolToOtherType { true = Wait, false = Continue }
+                  )
+                , ( "right click first ore asteroid"
+                  , firstAsteroidFromOverviewWindow
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonRight >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "and approach"
+                  , menuEntryInLastMenuContainingTextIgnoringCase "approach"
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "(for reliability wait until ship stopped)"
+                  , shipIsStopped
+                        >> Maybe.withDefault False
+                        >> mapBoolToOtherType { true = Continue, false = Wait }
+                  )
+                , ( "when in range right click ore asteroid"
+                  , firstAsteroidFromOverviewWindowInRange
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonRight >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "and lock target"
+                  , menuEntryInLastMenuContainingTextIgnoringCase "lock"
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+
+                -- Assume all visible modules are miners: Setup EVE Online client to hide any other module.
+                , ( "click first high slot with mining laser"
+                  , shipUiModules
+                        >> List.head
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "click second high slot with mining laser"
+                  , shipUiModules
+                        >> List.drop 1
+                        >> List.head
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "wait until ore hold full or asteroid depleted (both mining lasers stopped)"
+                  , \memoryMeasurement ->
+                        if memoryMeasurement |> isOreHoldFull |> Maybe.withDefault False then
+                            Continue
+
+                        else if memoryMeasurement |> shipUiModules |> List.all (.isActive >> (==) (Just False)) then
+                            JumpToSequence MineInBelt
+
+                        else
+                            Wait
+                  )
+                ]
+            , continueWith = TravelToStation
+            }
+
+        TravelToStation ->
+            { steps =
+                [ ( "Travel to station: open solar system menu"
+                  , .infoPanelCurrentSystem
+                        >> maybeNothingFromCanNotSeeIt
+                        >> Maybe.map (.listSurroundingsButton >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "Travel to station: open bookmark"
+                  , menuEntryInLastMenuContainingTextIgnoringCase stationBookmarkName
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                , ( "and dock"
+                  , menuEntryInLastMenuContainingTextIgnoringCase "dock"
+                        >> Maybe.map (.uiElement >> clickOnUIElement MouseButtonLeft >> ApplyEffectAndContinue)
+                        >> Maybe.withDefault Wait
+                  )
+                ]
+            , continueWith = TransferFromOreHold
+            }
+
+        TransferFromOreHold ->
+            { steps =
+                [ ( "click and hold to drag ore from ore hold to item hangar"
+                  , \memoryMeasurement ->
+                        case memoryMeasurement |> inventoryWindowItemHangar of
+                            Nothing ->
+                                Wait
+
+                            Just itemHangar ->
+                                case memoryMeasurement |> inventoryWindowSelectedContainerFirstItem of
+                                    Nothing ->
+                                        JumpToSequence TravelToAsteroidBelt
+
+                                    Just itemInInventory ->
+                                        { startLocation = itemInInventory.region |> centerFromRegion
+                                        , endLocation = itemHangar.region |> centerFromRegion
+                                        , mouseButton = MouseButtonLeft
+                                        }
+                                            |> Sanderling.SimpleDragAndDrop
+                                            |> ApplyEffectAndContinue
+                  )
+                ]
+
+            -- https://forum.botengine.org/t/how-to-automate-mining-asteroids-in-eve-online/628/44?u=viir
+            , continueWith = TransferFromOreHold
+            }
+
+
+initState : State
+initState =
+    SimpleSanderling.initState { remainingSequence = getProgramSequence TravelToAsteroidBelt }
+
+
+processEvent : InterfaceToHost.BotEvent -> State -> ( State, InterfaceToHost.BotResponse )
+processEvent =
+    SimpleSanderling.processEvent simpleProcessEvent
+
+
+simpleProcessEvent : BotEventAtTime -> SimpleState -> { newState : SimpleState, requests : List BotRequest, statusMessage : String }
+simpleProcessEvent eventAtTime stateBefore =
+    case eventAtTime.event of
+        SimpleSanderling.MemoryMeasurementCompleted memoryMeasurement ->
+            let
+                remainingSequenceBefore =
+                    stateBefore.remainingSequence
+
+                ( stepResult, stepDescription ) =
+                    -- 'remainingSequence' stores only the remaining steps, so we always execute the first of the steps in there.
+                    case remainingSequenceBefore.steps |> List.head of
+                        Nothing ->
+                            ( Continue, "Error: no step remaining" )
+
+                        Just ( currentStepDescription, decideBasedOnMemoryMeasurement ) ->
+                            ( decideBasedOnMemoryMeasurement memoryMeasurement, currentStepDescription )
+
+                advancedSequence =
+                    if (remainingSequenceBefore.steps |> List.length) > 0 then
+                        { remainingSequenceBefore | steps = remainingSequenceBefore.steps |> List.drop 1 }
+
+                    else
+                        remainingSequenceBefore.continueWith |> getProgramSequence
+
+                ( remainingSequence, effects ) =
+                    case stepResult of
+                        Continue ->
+                            ( advancedSequence, [] )
+
+                        ApplyEffectAndContinue effect ->
+                            ( advancedSequence, [ effect ] )
+
+                        Wait ->
+                            ( remainingSequenceBefore, [] )
+
+                        JumpToSequence sequenceName ->
+                            ( getProgramSequence sequenceName, [] )
+
+                effectsRequests =
+                    effects |> List.map EffectOnGameClientWindow
+
+                requests =
+                    effectsRequests ++ [ TakeMemoryMeasurementAfterDelayInMilliseconds generalStepDelayMilliseconds ]
+            in
+            { newState = { stateBefore | remainingSequence = remainingSequence }
+            , requests = requests
+            , statusMessage = stepDescription
+            }
+
+        SimpleSanderling.SetBotConfiguration botConfiguration ->
+            { newState = stateBefore
+            , requests = []
+            , statusMessage =
+                if botConfiguration |> String.isEmpty then
+                    ""
+
+                else
+                    "I have a problem with this configuration: I am not programmed to support configuration at all. Maybe the bot catalog (https://to.botengine.org/bot-catalog) has a bot which better matches your use case?"
+            }
+
+
+activeShipUiElementFromInventoryWindow : MemoryMeasurement -> Maybe UIElement
+activeShipUiElementFromInventoryWindow =
+    .inventoryWindow
+        >> maybeNothingFromCanNotSeeIt
+        >> Maybe.map .leftTreeEntries
+        -- Assume upmost entry is active ship.
+        >> Maybe.andThen (List.sortBy (.uiElement >> .region >> .top) >> List.head)
+        >> Maybe.map .uiElement
+
+
+shipUiModules : MemoryMeasurement -> List ShipUiModule
+shipUiModules =
+    .shipUi >> maybeNothingFromCanNotSeeIt >> Maybe.map .modules >> Maybe.withDefault []
+
+
+shipIsStopped : MemoryMeasurement -> Maybe Bool
+shipIsStopped =
+    .shipUi >> maybeNothingFromCanNotSeeIt >> Maybe.andThen .shipIsStopped
+
+
+menuEntryInLastMenuContainingTextIgnoringCase : String -> MemoryMeasurement -> Maybe SanderlingMemoryMeasurement.MemoryMeasurementMenuEntry
+menuEntryInLastMenuContainingTextIgnoringCase textToSearch =
+    menuEntryInLastMenuMatchingPredicate (.text >> String.toLower >> String.contains (textToSearch |> String.toLower))
+
+
+firstAsteroidFromOverviewWindow : MemoryMeasurement -> Maybe OverviewWindowEntry
+firstAsteroidFromOverviewWindow =
+    overviewWindowEntriesRepresentingAsteroids >> List.head
+
+
+firstAsteroidFromOverviewWindowInRange : MemoryMeasurement -> Maybe OverviewWindowEntry
+firstAsteroidFromOverviewWindowInRange =
+    firstAsteroidFromOverviewWindow
+        >> Maybe.andThen
+            (\asteroid ->
+                if asteroid |> overviewWindowEntryIsInRange |> Maybe.withDefault False then
+                    Just asteroid
+
+                else
+                    Nothing
+            )
+
+
+overviewWindowEntryIsInRange : OverviewWindowEntry -> Maybe Bool
+overviewWindowEntryIsInRange =
+    .distanceInMeters >> Result.map (\distanceInMeters -> distanceInMeters < 1000) >> Result.toMaybe
+
+
+overviewWindowEntriesRepresentingAsteroids : MemoryMeasurement -> List OverviewWindowEntry
+overviewWindowEntriesRepresentingAsteroids =
+    .overviewWindow
+        >> maybeNothingFromCanNotSeeIt
+        >> Maybe.map (.entries >> List.filter overviewWindowEntryRepresentsAnAsteroid)
+        >> Maybe.withDefault []
+
+
+overviewWindowEntryRepresentsAnAsteroid : OverviewWindowEntry -> Bool
+overviewWindowEntryRepresentsAnAsteroid entry =
+    (entry.textsLeftToRight |> List.any (String.toLower >> String.contains "asteroid"))
+        && (entry.textsLeftToRight |> List.any (String.toLower >> String.contains "belt") |> not)
+
+
+isOreHoldFull : MemoryMeasurement -> Maybe Bool
+isOreHoldFull =
+    oreHoldFillPercent
+        >> Maybe.map (\fillPercent -> fillPercent >= 99)
+
+
+oreHoldFillPercent : MemoryMeasurement -> Maybe Int
+oreHoldFillPercent =
+    .inventoryWindow
+        >> maybeNothingFromCanNotSeeIt
+        >> Maybe.andThen .selectedContainedCapacityGauge
+        >> Maybe.map (\capacity -> capacity.used * 100 // capacity.maximum)
+
+
+inventoryWindowSelectedContainerFirstItem : MemoryMeasurement -> Maybe UIElement
+inventoryWindowSelectedContainerFirstItem =
+    .inventoryWindow
+        >> maybeNothingFromCanNotSeeIt
+        >> Maybe.andThen .selectedContainerInventory
+        >> Maybe.andThen (.listViewItems >> List.head)
+
+
+inventoryWindowItemHangar : MemoryMeasurement -> Maybe UIElement
+inventoryWindowItemHangar =
+    .inventoryWindow
+        >> maybeNothingFromCanNotSeeIt
+        >> Maybe.map .leftTreeEntries
+        >> Maybe.andThen (List.filter (.text >> String.toLower >> String.contains "item hangar") >> List.head)
+        >> Maybe.map .uiElement
+
+
+menuEntryInLastMenuMatchingPredicate : (SanderlingMemoryMeasurement.MemoryMeasurementMenuEntry -> Bool) -> MemoryMeasurement -> Maybe SanderlingMemoryMeasurement.MemoryMeasurementMenuEntry
+menuEntryInLastMenuMatchingPredicate predicate =
+    .menus
+        >> List.reverse
+        >> List.head
+        >> Maybe.andThen (.entries >> List.filter predicate >> List.head)
+
+
+clickOnUIElement : MouseButton -> UIElement -> Sanderling.EffectOnWindowStructure
+clickOnUIElement mouseButton uiElement =
+    effectMouseClickAtLocation mouseButton (uiElement.region |> centerFromRegion)
+
+
+{-| The region of a ship entry in the inventory window can contain child nodes (e.g. 'Ore Hold').
+For this reason, we don't click on the center but stay close to the top.
+-}
+clickLocationOnInventoryShipEntry : UIElement -> Sanderling.Location2d
+clickLocationOnInventoryShipEntry uiElement =
+    { x = (uiElement.region.left + uiElement.region.right) // 2
+    , y = uiElement.region.top + 7
+    }
+
+
+infoPanelRouteFirstMarkerFromMemoryMeasurement : MemoryMeasurement -> Maybe InfoPanelRouteRouteElementMarker
+infoPanelRouteFirstMarkerFromMemoryMeasurement =
+    .infoPanelRoute
+        >> maybeNothingFromCanNotSeeIt
+        >> Maybe.map .routeElementMarker
+        >> Maybe.map (List.sortBy (\routeMarker -> routeMarker.uiElement.region.left + routeMarker.uiElement.region.top))
+        >> Maybe.andThen List.head
+
+
+isShipWarpingOrJumping : MemoryMeasurementShipUi -> Bool
+isShipWarpingOrJumping =
+    .indication
+        >> maybeNothingFromCanNotSeeIt
+        >> Maybe.andThen (.maneuverType >> maybeNothingFromCanNotSeeIt)
+        >> Maybe.map
+            (\maneuverType ->
+                [ SanderlingMemoryMeasurement.Warp, SanderlingMemoryMeasurement.Jump ]
+                    |> List.member maneuverType
+            )
+        -- If the ship is just floating in space, there might be no indication displayed.
+        >> Maybe.withDefault False
+
+
+mapBoolToOtherType : { true : a, false : a } -> Bool -> a
+mapBoolToOtherType { true, false } bool =
+    if bool then
+        true
+
+    else
+        false

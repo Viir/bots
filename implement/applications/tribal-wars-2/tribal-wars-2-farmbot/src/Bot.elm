b3a338ce7ae35152f216253179fa6f4f38005cd4
Michael Rätzel <viir@viir.de>
2020-03-24 06:32:08 +0000

    Integrate test results
    This variant with radius increased to 50 already ran for more than 10 hours in half a day: http://botcatalog.org/bot/8BDDCFEEE26432017E26EF23064EB62E984985A25A5EA9F51ACBFE954FB05CC2
    
    Danke an Taunzz für die Tests.

--- implement/applications/tribal-wars-2/tribal-wars-2-farmbot/src/Bot.elm
+++ implement/applications/tribal-wars-2/tribal-wars-2-farmbot/src/Bot.elm
@@ -1,4 +1,4 @@
-{- Tribal Wars 2 farmbot version 2020-03-22 🎉
+{- Tribal Wars 2 farmbot version 2020-03-24
    I search for barbarian villages around your villages and then attack them.
 
    When starting, I first open a new web browser window. This might take more on the first run because I need to download the web browser software.
@@ -106,7 +106,7 @@ selectedVillageInfoMaxAge =
 
 searchFarmsRadiusAroundOwnVillage : Int
 searchFarmsRadiusAroundOwnVillage =
-    30
+    50
 
 
 readFromGameTimeoutCountThresholdToRestart : Int

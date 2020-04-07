333f124dbbba52cad48d6974f825f01e5ef46350
jimburtoft <39492751+jimburtoft@users.noreply.github.com>
2020-04-06 23:20:31 -0400

    Parens to DescribeBranch

--- implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
+++ implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
@@ -309,7 +309,7 @@ inSpaceWithOreHoldSelected context seeUndockingComplete inventoryWindowWithOreHo
 
                     Just fillPercent ->
                         if context.settings.oreHoldMaxPercent <= fillPercent then
-                            DescribeBranch "The ore hold is over " ++ (context.settings.oreHoldMaxPercent |> String.fromInt) ++ "percent. Dock to station."
+                            DescribeBranch ("The ore hold is over " ++ (context.settings.oreHoldMaxPercent |> String.fromInt) ++ "percent. Dock to station.")
                                 (case context |> lastDockedStationNameFromInfoPanelFromMemoryOrSettings of
                                     Nothing ->
                                         DescribeBranch "At which station should I dock?. I was never docked in a station in this session." (EndDecisionPath Wait)

b0c88ff2deb58acab20d46f38f2028e3a4d52006
Michael Rätzel <viir@viir.de>
2020-04-10 07:40:46 +0000

    Fix bug in reporting unloaded ore
    Do not decrease this metric during mining.

--- implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
+++ implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online mining bot version 2020-04-08
+{- EVE Online mining bot version 2020-04-10
 
    The bot warps to an asteroid belt, mines there until the ore hold is full, and then docks at a station to unload the ore. It then repeats this cycle until you stop it.
    It remembers the station in which it was last docked, and docks again at the same station.
@@ -884,7 +884,8 @@ integrateCurrentReadingsIntoBotMemory currentReading botMemoryBefore =
                             0
 
                         Just currentUsedCapacityInOreHold ->
-                            previousUsedCapacityInOreHold - currentUsedCapacityInOreHold
+                            -- During mining, when new ore appears in the inventory, this difference is negative.
+                            max 0 (previousUsedCapacityInOreHold - currentUsedCapacityInOreHold)
 
         timesUnloaded =
             botMemoryBefore.timesUnloaded

1f915f4583cde98e0491e66bc73d7df0e92d1aac
John <john-dev@botengine.email>
2020-04-11 10:48:04 +0000

    Guide users
    Clarify the bot uses drones if available.

--- implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
+++ implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online mining bot version 2020-04-10
+{- EVE Online mining bot version 2020-04-11
 
    The bot warps to an asteroid belt, mines there until the ore hold is full, and then docks at a station to unload the ore. It then repeats this cycle until you stop it.
    It remembers the station in which it was last docked, and docks again at the same station.
@@ -12,6 +12,7 @@
      + Place all mining modules (to activate on targets) in the top row.
      + Place modules that should always be active in the middle row.
      + Hide passive modules by disabling the check-box `Display Passive Modules`.
+   + In case the bot should use drones, place them in the drone bay and open the 'Drones' window.
 -}
 {-
    bot-catalog-tags:eve-online,mining

e05ec9e78494cd1c1b42bd12f583bb46401f7b57
John <john-dev@botengine.email>
2020-04-11 10:21:35 +0000

    Copy launchAndEngageDrones from the ratting bot
    From https://github.com/Viir/bots/blob/26af7322847bd406117b6bfb94c6e074a328be3b/implement/applications/eve-online/eve-online-anomaly-ratting-bot/src/Bot.elm

--- implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
+++ implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
@@ -627,6 +627,75 @@ dockToRandomStation parsedUserInterface =
         parsedUserInterface
 
 
+launchAndEngageDrones : ParsedUserInterface -> Maybe DecisionPathNode
+launchAndEngageDrones parsedUserInterface =
+    parsedUserInterface.dronesWindow
+        |> maybeNothingFromCanNotSeeIt
+        |> Maybe.andThen
+            (\dronesWindow ->
+                case ( dronesWindow.droneGroupInBay, dronesWindow.droneGroupInLocalSpace ) of
+                    ( Just droneGroupInBay, Just droneGroupInLocalSpace ) ->
+                        let
+                            idlingDrones =
+                                droneGroupInLocalSpace.drones
+                                    |> List.filter (.uiNode >> .uiNode >> EveOnline.ParseUserInterface.getAllContainedDisplayTexts >> List.any (String.toLower >> String.contains "idle"))
+
+                            dronesInBayQuantity =
+                                droneGroupInBay.header.quantityFromTitle |> Maybe.withDefault 0
+
+                            dronesInLocalSpaceQuantity =
+                                droneGroupInLocalSpace.header.quantityFromTitle |> Maybe.withDefault 0
+                        in
+                        if 0 < (idlingDrones |> List.length) then
+                            Just
+                                (DescribeBranch "Engage idling drone(s)"
+                                    (EndDecisionPath
+                                        (Act
+                                            { actionsAlreadyDecided =
+                                                ( "Rightclick on the drones group."
+                                                , [ droneGroupInLocalSpace.header.uiNode |> clickOnUIElement MouseButtonRight ]
+                                                )
+                                            , actionsDependingOnNewReadings =
+                                                [ ( "Click menu entry 'engage target'."
+                                                  , lastContextMenuOrSubmenu
+                                                        >> Maybe.andThen (menuEntryContainingTextIgnoringCase "engage target")
+                                                        >> Maybe.map (.uiNode >> clickOnUIElement MouseButtonLeft >> List.singleton)
+                                                  )
+                                                ]
+                                            }
+                                        )
+                                    )
+                                )
+
+                        else if 0 < dronesInBayQuantity && dronesInLocalSpaceQuantity < 5 then
+                            Just
+                                (DescribeBranch "Launch drones"
+                                    (EndDecisionPath
+                                        (Act
+                                            { actionsAlreadyDecided =
+                                                ( "Right click on the drones group."
+                                                , [ droneGroupInBay.header.uiNode |> clickOnUIElement MouseButtonRight ]
+                                                )
+                                            , actionsDependingOnNewReadings =
+                                                [ ( "Click menu entry 'Launch drone'."
+                                                  , lastContextMenuOrSubmenu
+                                                        >> Maybe.andThen (menuEntryContainingTextIgnoringCase "Launch drone")
+                                                        >> Maybe.map (.uiNode >> clickOnUIElement MouseButtonLeft >> List.singleton)
+                                                  )
+                                                ]
+                                            }
+                                        )
+                                    )
+                                )
+
+                        else
+                            Nothing
+
+                    _ ->
+                        Nothing
+            )
+
+
 actWithoutFurtherReadings : ( String, List VolatileHostInterface.EffectOnWindowStructure ) -> EndDecisionPathStructure
 actWithoutFurtherReadings actionsAlreadyDecided =
     Act { actionsAlreadyDecided = actionsAlreadyDecided, actionsDependingOnNewReadings = [] }

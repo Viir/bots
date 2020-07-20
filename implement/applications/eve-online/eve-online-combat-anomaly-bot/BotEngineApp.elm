711676456c3aa07ebb539e32d3dbb18352bdcb70
Michael Rätzel <viir@viir.de>
2020-07-20 11:05:18 +0000

    Add shortcut to security level in location info panel

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/BotEngineApp.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2020-07-18
+{- EVE Online combat anomaly bot version 2020-07-20
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 
    Setup instructions for the EVE Online client:

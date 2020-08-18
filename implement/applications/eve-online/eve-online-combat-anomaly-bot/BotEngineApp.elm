57c40647015383c8915ebacff527581bbbaea9e4
Michael Rätzel <viir@viir.de>
2020-08-18 10:06:23 +0000

    Shortcuts to directional scanner by Hi Fi
    Add shortcuts to the directional scanner discovered by Hi Fi (hifig) yesterday at https://forum.botengine.org/t/using-direction-scan-before-warp-to-any-anomaly/3523/5?u=viir
    
    The directory linked by Hi Fi contains these samples:
    
    + `Result to ignore session-2020-08-17T14-46-05-231519-event-90-eve-online-memory-reading.json`
    + `Result to warp in session-2020-08-17T14-46-05-231519-event-104-eve-online-memory-reading.json`

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/BotEngineApp.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2020-08-15
+{- EVE Online combat anomaly bot version 2020-08-18
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 
    Setup instructions for the EVE Online client:

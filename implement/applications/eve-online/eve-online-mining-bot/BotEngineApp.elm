dea59f373d7be5f3908ec1e2ff24a5309fbb7617
Michael Rätzel <viir@viir.de>
2020-12-28 19:51:11 +0000

    Guide users to configure the overview window in the game client
    Avoid the issue described by nexty5870 at https://github.com/Viir/bots/issues/31
    
    Draw the user's attention to this part of the game client's setup: Add a branch to ask the user to enable the 'Name' column in the overview window. Other functions in this app use the name field to classify overview entries as asteroids.

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -1,4 +1,4 @@
-{- EVE Online mining bot version 2020-12-13
+{- EVE Online mining bot version 2020-12-28
    The bot warps to an asteroid belt, mines there until the ore hold is full, and then docks at a station or structure to unload the ore. It then repeats this cycle until you stop it.
    If no station name or structure name is given with the app-settings, the bot docks again at the station where it was last docked.
 
@@ -204,9 +204,15 @@ miningBotDecisionRoot context =
                             }
                             context
                             |> Maybe.withDefault
-                                (ensureOreHoldIsSelectedInInventoryWindow
-                                    context.readingFromGameClient
-                                    (inSpaceWithOreHoldSelected context seeUndockingComplete)
+                                (ensureUserEnabledNameColumnInOverview
+                                    { ifEnabled =
+                                        ensureOreHoldIsSelectedInInventoryWindow
+                                            context.readingFromGameClient
+                                            (inSpaceWithOreHoldSelected context seeUndockingComplete)
+                                    , ifDisabled =
+                                        describeBranch "Please configure the overview to show objects names." askForHelpToGetUnstuck
+                                    }
+                                    seeUndockingComplete
                                 )
                 }
                 context.readingFromGameClient
@@ -1083,6 +1089,18 @@ clickModuleButtonButWaitIfClickedInPreviousStep context moduleButton =
                 , moduleButton.uiNode |> clickOnUIElement MouseButtonLeft
                 )
             )
+
+
+ensureUserEnabledNameColumnInOverview : { ifEnabled : DecisionPathNode, ifDisabled : DecisionPathNode } -> SeeUndockingComplete -> DecisionPathNode
+ensureUserEnabledNameColumnInOverview { ifEnabled, ifDisabled } seeUndockingComplete =
+    if
+        (seeUndockingComplete.overviewWindow.entries |> List.all (.objectName >> (==) Nothing))
+            && (0 < List.length seeUndockingComplete.overviewWindow.entries)
+    then
+        describeBranch "The 'Name' column in the overview window seems disabled." ifDisabled
+
+    else
+        ifEnabled
 
 
 activeShipTreeEntryFromInventoryWindow : EveOnline.ParseUserInterface.InventoryWindow -> Maybe EveOnline.ParseUserInterface.InventoryWindowLeftTreeEntry

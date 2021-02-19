760f6684995e4493d9b4d04fd8c6a74ee1f21768
Michael Rätzel <viir@viir.de>
2021-02-19 17:43:26 +0000

    Add shortcut to window discovered by artsevruk last week
    Add shortcuts to UI nodes of the 'Abyssal'/'key activation' window discovered by artsevruk on 2021-02-12 at https://forum.botengine.org/t/abyssal-window/3848/5
    
    Implement shortcuts to UI nodes based on `session-2021-02-12T15-03-53-6366a4.zip-event-23-eve-online-memory-reading.json`
    
    Also, add a shortcut to the activation button seen in the screenshot shared by artsevruk at https://forum.botengine.org/t/abyssal-window/3848/3

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/BotEngineApp.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2021-02-06
+{- EVE Online combat anomaly bot version 2021-02-19
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 
    Setup instructions for the EVE Online client:

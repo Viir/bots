0868deb15b5a9cc1b4e2757b45b5cd7fc3478c24
Michael Rätzel <viir@viir.de>
2021-09-17 16:34:11 +0000

    Support navigating guides for EVE Online
    + Expand the documentation comments in modules. Add links to the most important guides on EVE Online.
    + Place the documentation comments so that popular integrated development environments pick them up and show them in tooltips for completion suggestions or when hovering over references.

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2021-08-30
+{- EVE Online combat anomaly bot version 2021-09-17
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 
    Setup instructions for the EVE Online client:
--- implement/applications/eve-online/eve-online-warp-to-0-autopilot/Bot.elm
+++ implement/applications/eve-online/eve-online-warp-to-0-autopilot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online Warp-to-0 auto-pilot version 2021-08-30
+{- EVE Online Warp-to-0 auto-pilot version 2021-09-17
    This bot makes your travels faster and safer by directly warping to gates/stations. It follows the route set in the in-game autopilot and uses the context menu to initiate jump and dock commands.
 
    Before starting the bot, set up the game client as follows:

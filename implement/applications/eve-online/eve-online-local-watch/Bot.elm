9706b9d2f4846126145de06deaf99bb201f6c4e2
Michael Rätzel <viir@viir.de>
2021-10-06 15:13:54 +0000

    Implement EVE Online bot for 2021-10-05 scenario of chuckyone
    Implement for the scenario shared by chuckyone at https://forum.botlab.org/t/how-to-add-audio-message-if-neutral-or-enemy-hits-local/93/24
    
    From memory reading, we already have the region where the UI element for the pilot is. We can use that to decide where to check the colors.
    I found this color in the screenshot you posted: red=5/255, green=34/255, blue=120/255.
    
    With the current training data set, the classification itself is simple:
    
    + For each pixel, compare the color to the one we found in the sample to classify that pixel as blue or not blue.
    + If any of the pixels in the UI element for the pilot is blue, we consider the pilot a friend.
    
    The colors can vary for several reasons. Therefore we do not check the color for equality but use a more inclusive check.
    In the scenario you described above, it is enough to require the blue component to be more than twice as intense as green and red.

--- implement/applications/eve-online/eve-online-local-watch/Bot.elm
+++ implement/applications/eve-online/eve-online-local-watch/Bot.elm
@@ -1,9 +1,11 @@
-{- EVE Online Intel Bot - Local Watch Script - 2021-09-21
-   This bot watches local and plays an alarm sound when a pilot with bad standing appears.
+{- EVE Online Intel Bot - chuckyone Local Watch - 2021-10-06
+
+   This bot watches the local chat window and plays an alarm sound when more than one pilot without blue color appears.
+   The classification into blue and not blue is based on the sample shared by chuckyone at https://forum.botlab.org/t/how-to-add-audio-message-if-neutral-or-enemy-hits-local/93/20
 -}
 {-
-   bot-catalog-tags:eve-online,intel,local-watch
-   authors-forum-usernames:viir
+   catalog-tags:eve-online,local-watch
+   authors-forum-usernames:viir,chuckyone
 -}
 
 
@@ -15,6 +17,7 @@ module Bot exposing
 import BotLab.BotInterface_To_Host_20210823 as InterfaceToHost
 import Common.AppSettings as AppSettings
 import Common.EffectOnWindow exposing (MouseButton(..))
+import Dict
 import EveOnline.BotFramework
     exposing
         ( BotEventResponseEffect(..)
@@ -22,11 +25,7 @@ import EveOnline.BotFramework
         , UseContextMenuCascadeNode(..)
         , localChatWindowFromUserInterface
         )
-
-
-goodStandingPatterns : List String
-goodStandingPatterns =
-    [ "good standing", "excellent standing", "is in your" ]
+import EveOnline.ParseUserInterface
 
 
 type alias BotState =
@@ -65,25 +64,58 @@ processEveOnlineBotEvent :
     -> ( BotState, EveOnline.BotFramework.BotEventResponse )
 processEveOnlineBotEvent eventContext event stateBefore =
     case event of
-        EveOnline.BotFramework.ReadingFromGameClientCompleted parsedUserInterface _ ->
+        EveOnline.BotFramework.ReadingFromGameClientCompleted parsedUserInterface readingFromGameClientImage ->
             let
                 ( effects, statusMessage ) =
-                    botEffectsFromGameClientState parsedUserInterface
+                    botEffectsFromGameClientState parsedUserInterface readingFromGameClientImage
             in
             ( stateBefore
             , EveOnline.BotFramework.ContinueSession
                 { effects = effects
                 , millisecondsToNextReadingFromGame = 2000
                 , statusDescriptionText = statusMessage
-                , screenshotRegionsToRead = always { rects1x1 = [] }
+                , screenshotRegionsToRead = screenshotRegionsToRead
                 }
             )
 
 
-botEffectsFromGameClientState : ReadingFromGameClient -> ( List BotEventResponseEffect, String )
-botEffectsFromGameClientState parsedUserInterface =
+screenshotRegionsToRead : ReadingFromGameClient -> { rects1x1 : List EveOnline.BotFramework.Rect2dStructure }
+screenshotRegionsToRead parsedUserInterface =
+    let
+        rects1x1 =
+            parsedUserInterface
+                |> getPilotsWithRegionToReadInImage
+                |> Result.map (List.map Tuple.second)
+                |> Result.withDefault []
+    in
+    { rects1x1 = rects1x1 }
+
+
+getPilotsWithRegionToReadInImage : ReadingFromGameClient -> Result String (List ( EveOnline.ParseUserInterface.ChatUserEntry, EveOnline.BotFramework.Rect2dStructure ))
+getPilotsWithRegionToReadInImage parsedUserInterface =
     case parsedUserInterface |> localChatWindowFromUserInterface of
         Nothing ->
+            Err "I don't see the local chat window."
+
+        Just localChatWindow ->
+            localChatWindow.userlist
+                |> Maybe.map .visibleUsers
+                |> Maybe.withDefault []
+                |> List.map
+                    (\chatUser ->
+                        let
+                            originalRegion =
+                                chatUser.uiNode.totalDisplayRegion
+                        in
+                        ( chatUser, { originalRegion | width = 20 } )
+                    )
+                |> Ok
+
+
+botEffectsFromGameClientState : ReadingFromGameClient -> EveOnline.BotFramework.ReadingFromGameClientImage -> ( List BotEventResponseEffect, String )
+botEffectsFromGameClientState parsedUserInterface readingFromGameClientImage =
+    case getPilotsWithRegionToReadInImage parsedUserInterface of
+        Err error ->
             ( [ EveOnline.BotFramework.EffectConsoleBeepSequence
                     [ { frequency = 700, durationInMs = 100 }
                     , { frequency = 0, durationInMs = 100 }
@@ -91,38 +123,42 @@ botEffectsFromGameClientState parsedUserInterface =
                     , { frequency = 400, durationInMs = 100 }
                     ]
               ]
-            , "I don't see the local chat window."
+            , error
             )
 
-        Just localChatWindow ->
+        Ok visibleUsers ->
             let
-                chatUserHasGoodStanding chatUser =
-                    goodStandingPatterns
-                        |> List.any
-                            (\goodStandingPattern ->
-                                chatUser.standingIconHint
-                                    |> Maybe.map (String.toLower >> String.contains goodStandingPattern)
-                                    |> Maybe.withDefault False
-                            )
+                isBlue pixelValue =
+                    pixelValue.red * 2 < pixelValue.blue && pixelValue.green * 2 < pixelValue.blue
 
-                visibleUsers =
-                    localChatWindow.userlist
-                        |> Maybe.map .visibleUsers
-                        |> Maybe.withDefault []
+                chatUserIsBlue ( chatUser, regionToCheck ) =
+                    let
+                        pixelsInRegionToCheck =
+                            List.range regionToCheck.y (regionToCheck.y + regionToCheck.height)
+                                |> List.concatMap
+                                    (\y ->
+                                        List.range regionToCheck.x (regionToCheck.x + regionToCheck.width)
+                                            |> List.filterMap
+                                                (\x ->
+                                                    readingFromGameClientImage.pixels1x1 |> Dict.get ( x, y )
+                                                )
+                                    )
+                    in
+                    List.any isBlue pixelsInRegionToCheck
 
-                subsetOfUsersWithNoGoodStanding =
+                subsetOfUsersNotBlue =
                     visibleUsers
-                        |> List.filter (chatUserHasGoodStanding >> not)
+                        |> List.filter (chatUserIsBlue >> not)
 
                 chatWindowReport =
                     "I see "
                         ++ (visibleUsers |> List.length |> String.fromInt)
                         ++ " users in the local chat. "
-                        ++ (subsetOfUsersWithNoGoodStanding |> List.length |> String.fromInt)
-                        ++ " with no good standing."
+                        ++ (subsetOfUsersNotBlue |> List.length |> String.fromInt)
+                        ++ " not blue."
 
                 alarmRequests =
-                    if 1 < (subsetOfUsersWithNoGoodStanding |> List.length) then
+                    if 1 < (subsetOfUsersNotBlue |> List.length) then
                         [ EveOnline.BotFramework.EffectConsoleBeepSequence
                             [ { frequency = 700, durationInMs = 100 }
                             , { frequency = 0, durationInMs = 100 }

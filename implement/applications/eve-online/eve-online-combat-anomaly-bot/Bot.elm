f69c066c1d1a156a56d944d689a4f52286ab14d6
Michael Rätzel <viir@viir.de>
2022-11-08 22:11:16 +0000

    Adapt to the new EVE Online client shared by Davina today
    Adapt to a variant of the EVE Online game client found in the sample `process-sample-8668424216.zip` shared by Davina today.

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2022-10-29
+{- EVE Online combat anomaly bot version 2022-11-08
 
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 

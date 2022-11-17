0b4212ab37d41426c1d1a0c26cff93512f170081
Michael Rätzel <viir@viir.de>
2022-11-17 09:03:42 +0000

    Adapt to the sample shared by briancorner last week
    Adapt to sample 'session-2022-11-10T16-24-29-event-46' contributed by briancorner this week.

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2022-11-08
+{- EVE Online combat anomaly bot version 2022-11-17
 
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 

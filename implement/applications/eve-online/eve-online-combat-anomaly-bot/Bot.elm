dd02b92bd304c47ff6d3b0ed5d4898893c3ab3bb
Michael Rätzel <viir@viir.de>
2023-01-05 12:42:55 +0000

    Add shortcuts to ship heat gauges and heat status tooltip
    Add shortcuts into memory reading based on the observations shared by Fabian Locker this week.

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2023-01-03
+{- EVE Online combat anomaly bot version 2023-01-04
 
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 

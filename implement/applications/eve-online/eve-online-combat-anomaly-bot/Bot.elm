c4a403274ab7f8cfbc01829925348d4363811fb0
Michael Rätzel <viir@viir.de>
2023-02-20 18:46:56 +0000

    Parse button Stack All location Inventory Window
    Transfer from https://github.com/Arcitectus/Sanderling/commit/954869df272419c4d08e122fa06aa24b28fcc4a3

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2023-02-13
+{- EVE Online combat anomaly bot version 2023-02-17
 
    This bot uses the probe scanner to warp to combat anomalies and kills rats using drones and weapon modules.
 

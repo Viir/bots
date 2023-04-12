498a65f562fa6c77fa872b09998969f183de7e25
Michael Rätzel <viir@viir.de>
2023-04-12 10:54:06 +0000

    Fix syntax error

--- implement/applications/eve-online/eve-online-warp-to-0-autopilot/Bot.elm
+++ implement/applications/eve-online/eve-online-warp-to-0-autopilot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online warp-to-0 auto-pilot version 2023-04-07
+{- EVE Online warp-to-0 auto-pilot version 2023-04-12
 
    This bot makes your travels faster and safer by directly warping to gates/stations. It follows the route set in the in-game autopilot and uses the context menu to initiate jump and dock commands.
 
@@ -70,7 +70,7 @@ parseBotSettings : String -> Result String BotSettings
 parseBotSettings =
     AppSettings.parseSimpleListOfAssignmentsSeparatedByNewlines
         ([ ( "activate-module-always"
-           , { description = "Text found in tooltips of ship modules that should always be active. For example: " cloaking device "."
+           , { description = "Text found in tooltips of ship modules that should always be active. For example: 'cloaking device'."
              , valueParser =
                 AppSettings.valueTypeString
                     (\moduleName settings ->

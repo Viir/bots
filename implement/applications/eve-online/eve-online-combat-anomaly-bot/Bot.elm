c49e0a895480323d3bfe584481017e75439946a0
Michael Rätzel <viir@viir.de>
2023-04-18 09:16:09 +0000

    Fix bug in computing time to request next reading of game client

--- implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
+++ implement/applications/eve-online/eve-online-combat-anomaly-bot/Bot.elm
@@ -1,4 +1,4 @@
-{- EVE Online combat anomaly bot version 2023-04-08
+{- EVE Online combat anomaly bot version 2023-04-18
 
    This bot uses the probe scanner to find combat anomalies and kills rats using drones and weapon modules.
 
